b2088c06b1942443786804cd25c24ac62df9163e
Divyen-Dalwadi2000 <divyen81@gmail.com>
2024-08-11 13:50:43 +0530

    Implement View Available Books Test

--- src/LibraryTest.java
+++ src/LibraryTest.java
@@ -49,4 +49,20 @@ public class LibraryTest {
         assertTrue(library.getAvailableBooks().contains(book));
         assertFalse(library.getBorrowedBooks().contains(book));
     }
+
+    @Test
+    public void testViewAvailableBooks() {
+        Book book1 = new Book("81-203-1234-5", "Ruby", "Yukihiro Matsumoto", 2008);
+        Book book2 = new Book("81-203-1234-6", "Java", "Pravin Jain", 2010);
+        Book book3 = new Book("81-203-1234-7", "React", "Alex Banks", 2020);
+        Book book4 = new Book("81-203-1234-8", "Test-Driven Development", "Kent Beck", 2000);
+
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
+
+        assertTrue(library.getAvailableBooks().contains(book2));
+        assertTrue(library.getAvailableBooks().contains(book4));
+    }
 }
